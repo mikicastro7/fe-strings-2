19a27520e1d780c9e983749fed3edb8ada0f1b14
Miquel Castro <castro-miquel@hotmail.com>
2021-02-18 22:29:00 +0100

    quito la variable palabra

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const snakeCaseText = "hola_me_llamo_miquel"
 
 const camelCaseText = snakeCaseText.split("_").map((ele, i) => {
-    let palabra;
-    i !== 0 ? palabra = ele[0].toUpperCase() + ele.slice(1) : palabra = ele;
-    return palabra
+    return i !== 0 ? ele[0].toUpperCase() + ele.slice(1) : ele;
 }).join("")
 
 console.log(camelCaseText)
