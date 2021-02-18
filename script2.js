a13dda086b5f4d62e3d4ced60e29370c90aec0ca
Miquel Castro <castro-miquel@hotmail.com>
2021-02-18 22:34:40 +0100

    quito param i del map

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 const camelCaseTextEx2 = "holaMeLlamoMiquel";
 
-const snakeCaseTextEx2 = camelCaseText.split(/(?=[A-Z])/).map((ele, i) => {
+const snakeCaseTextEx2 = camelCaseText.split(/(?=[A-Z])/).map(ele => {
     return ele[0].toLowerCase() + ele.slice(1)
 }).join("_");
 
