f3551f5a13f36916a3362eee17b925e2cfaccc6a
Miquel Castro <castro-miquel@hotmail.com>
2021-02-18 22:26:44 +0100

    Quito varias lineas de codigo iunecesarias en los ejercicios

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
 const camelCaseTextEx2 = "holaMeLlamoMiquel";
 
-const snakeCaseTextEx2 = camelCaseText.split(/(?=[A-Z])/).map((ele, i) => {
-    return ele[0].toLowerCase() + ele.slice(1)
-}).join("_");
+const snakeCaseTextEx2 = camelCaseText.split(/(?=[A-Z])/).map
+(ele =>ele[0].toLowerCase() + ele.slice(1)).join("_");
 
-console.log(snakeCaseTextEx2)
+console.log(snakeCaseTextEx2);
